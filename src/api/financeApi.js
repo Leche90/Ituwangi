ebdc2441c2056f1949540cf5994d930b78bd58e7
Lanzema Leche <lleche@academic.rrc.ca>
2025-09-21 14:58:31 -0500

    updated financeApi

--- src/api/financeApi.js
+++ src/api/financeApi.js
@@ -1,7 +1,19 @@
-const financeApi = {
-  getEarnings: async () => {
-    return await fetch("/api/finance/earnings").then((res) => res.json());
-  },
+import axios from "axios";
+
+const API_URL = "http://localhost:3000/finance";
+
+// Get earnings overview
+const getEarnings = async () => {
+  try {
+    const response = await axios.get(`${API_URL}/earnings`);
+    return response.data;
+  } catch (error) {
+    console.error("Failed to fetch earnings:", error);
+    return [];
+  }
 };
 
+export { getEarnings };
+
+const financeApi = { getEarnings };
 export default financeApi;
